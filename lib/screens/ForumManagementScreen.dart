5b53db022e8dce852fa6c4c3a4048456b5e9a1e0
Chell2003 <105116183+Chell2003@users.noreply.github.com>
2025-09-19 15:21:24 +0800

    hello

--- lib/screens/ForumManagementScreen.dart
+++ lib/screens/ForumManagementScreen.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../components/sidebar.dart';
 import '../../providers/forum_provider.dart';
-import '../../providers/notification_provider.dart';
 
 class ForumManagementScreen extends StatefulWidget {
   const ForumManagementScreen({super.key});
